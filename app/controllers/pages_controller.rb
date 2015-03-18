cbc20461a7c86e44cbcf7157adf36e71b05c427e
Patrick <pfg164@yahoo.com>
2015-03-18 21:19:24 +0000

    Initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+    
+    before_action :require_user, except: [:contact, :help]
+    
+    def contact
+    end
+    
+    def help
+    end
+    
+    def about
+    end
+    
+end
